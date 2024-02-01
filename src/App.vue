53e69b684360b4e7092e3af5e93b7bfde57e2231
songguo <202221165028@njtech.edu.cn>
2024-02-01 20:16:01 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+<el-container>
+  <el-header>webgis系统</el-header>
+  <el-container>
+    <el-aside width="200px">菜单栏</el-aside>
+    <el-main>
+      <MapView/>
+    </el-main>
+  </el-container>
+</el-container>
+</template>
+
+<script>
+import MapView from "./components/MapView.vue"
+export default {
+  name: 'App',
+  components: {MapView},
+};
+
+</script>
+<style>
+  .el-container{
+    height:100vh
+  }
+
+  .el-header, .el-footer {
+    background-color: #B3C0D1;
+    color: #333;
+    text-align: center;
+    line-height: 60px;
+  } 
+
+  .el-aside {
+    background-color: #062344;
+    color: #f8f7f7;
+    text-align: center;
+    line-height: 200px;
+  } 
+
+  .el-main {
+    background-color: #f6f8fb;
+    color: #333;
+    text-align: center;
+    line-height: 160px;
+  }
+</style>
---
+++ src/components/MapView.vue
@@ -0,0 +1,41 @@
+<template>
+    <div id="mapview"></div>
+  </template>
+  
+  <script>
+  import { loadModules } from 'esri-loader';
+  const options = {
+    url: 'https://js.arcgis.com/4.18/init.js',
+    css: 'https://js.arcgis.com/4.18/esri/themes/light/main.css',
+  };
+  export default {
+    name: 'MapView',
+    components: {},
+    mounted: function () {
+      this._createMapView();
+    },
+    methods: {
+      async _createMapView() {
+        const [Map, MapView] = await loadModules(['esri/Map', 'esri/views/MapView'], options);
+        const map = new Map({
+          basemap: 'osm',
+        });
+        const view = new MapView({
+          container: 'mapview',
+          center: [108, 32],
+          map: map,
+          zoom: 5,
+        });
+        console.log(view);
+      },
+    },
+  };
+  </script>
+  
+  <style>
+  #mapview {
+    position: relative;
+    width: 100%;
+    height: 100%;
+  }
+  </style>
